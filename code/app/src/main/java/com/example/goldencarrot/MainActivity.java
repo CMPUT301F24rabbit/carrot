646f232c1e3fb6f949bbf098d7b1ef3dfd47d0c6
jctuason <jctuason@ualberta.com>
2024-10-27 12:58:57 -0600

    admin view all users

---
+++ code/app/src/main/java/com/example/goldencarrot/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.goldencarrot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.goldencarrot.data.db.UserRepository;
+import com.example.goldencarrot.data.model.user.UserUtils;
+import com.example.goldencarrot.views.AdminHomeActivity;
+import com.example.goldencarrot.views.EntrantHomeView;
+import com.example.goldencarrot.views.OrganizerHomeView;
+import com.example.goldencarrot.views.SelectUserTypeActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        UserRepository userRepository = new UserRepository();
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        userRepository.getUserTypeFromFirestore(new UserRepository.FirestoreCallbackUserType() {
+            @Override
+            public void onSuccess(String userType) {
+                // Handle the retrieved user type
+                navigateByUserType(userType);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                // Handle any errors
+            }
+        });
+
+        //  Login Button
+        findViewById(R.id.auth_login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Sign-Up Button
+        findViewById(R.id.auth_sign_up_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SelectUserTypeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void navigateByUserType(String userType) {
+        Intent intent;
+        if (userType.equals(UserUtils.ADMIN_TYPE)) {
+            intent = new Intent(MainActivity.this, AdminHomeActivity.class);
+            startActivity(intent);
+        }
+
+        if (userType.equals(UserUtils.ORGANIZER_TYPE)) {
+            intent = new Intent(MainActivity.this, OrganizerHomeView.class);
+            startActivity(intent);
+        }
+
+        if (userType.equals(UserUtils.PARTICIPANT_TYPE)) {
+            intent = new Intent(MainActivity.this, EntrantHomeView.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ code/app/src/main/java/com/example/goldencarrot/data/db/UserRepository.java
@@ -0,0 +1,139 @@
+package com.example.goldencarrot.data.db;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.goldencarrot.data.model.user.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Queries User DB
+ */
+public class UserRepository {
+    private static final String TAG = "DB" ;
+    private final FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+
+    private CollectionReference userCollection;
+    private List<DocumentSnapshot> listOfUsers;
+
+    public UserRepository() {
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    /**
+     * Todo Implemet this method
+     * @param user
+     */
+    // Add user to Firestore
+    public void addUser(User user, String uid) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("email", user.getEmail());
+        userData.put("userType", user.getUserType());
+        userData.put("username", user.getUsername());
+
+
+        Log.d(TAG, "Email: " + user.getEmail());
+        Log.d(TAG, "User Type: " + user.getUserType());
+        Log.d(TAG, "Username: " + user.getUsername());
+
+        // Add the user document to the "users" collection using their UID as the document ID
+        db.collection("users").document(uid)
+                .set(userData)
+                .addOnSuccessListener(aVoid -> {
+                    // Successfully added the user
+                    System.out.println("User added to Firestore");
+                })
+                .addOnFailureListener(e -> {
+                    // Handle the error
+                    System.err.println("Error adding user to Firestore: " + e.getMessage());
+                    Log.e(TAG, "Error adding user to Firestore", e);
+
+                });
+    }
+
+    /**
+     * Retrieves the current user's type from the Firestore users collection.
+     *
+     * @param callback a callback to handle the result (userType)
+     */
+    public void getUserTypeFromFirestore(FirestoreCallbackUserType callback) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();  // Get the authenticated user's UID
+
+            // Reference to the user's document in the "users" collection
+            DocumentReference userRef = db.collection("users").document(uid);
+
+            userRef.get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            // Retrieve the userType from the Firestore document
+                            String userType = documentSnapshot.getString("userType");
+                            callback.onSuccess(userType);  // Pass the userType to the callback
+                        } else {
+                            callback.onFailure(new Exception("User document does not exist"));
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        callback.onFailure(e);
+                    });
+
+        } else {
+            callback.onFailure(new Exception("No authenticated user found"));
+        }
+    }
+
+    /**
+     * Callback interface to handle Firestore query results for userType.
+     */
+    public interface FirestoreCallbackUserType {
+        void onSuccess(String userType);
+        void onFailure(Exception e);
+    }
+    /**
+     * Retrieves all users from the Firestore user collection
+     * @param callback a callback to handle the result
+     */
+    public void getAllUsersFromFirestore(FirestoreCallbackAllUsers callback) {
+        userCollection = db.collection("users");
+        // get all documents, code from user TomH, downloaded 24/10/24:
+        // https://stackoverflow.com/questions/50727254/how-to-retrieve-all-documents-from-a-collection-in-firestore
+        userCollection.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            listOfUsers = task.getResult().getDocuments();
+                            callback.onSuccess(listOfUsers);
+                        } else {
+                            callback.onFailure(new Exception("No users found"));
+                        }
+                    }
+                });
+    }
+    /**
+     * Callback interface to handle Firestore query results for users collection
+     */
+    public interface FirestoreCallbackAllUsers {
+        void onSuccess(List<DocumentSnapshot> listOfUsers);
+        void onFailure(Exception e);
+    }
+}
