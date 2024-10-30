b6a0cc2f76c9cecb6f2940c637329007c4687384
jctuason <jctuason@ualberta.com>
2024-10-30 15:01:49 -0600

    Admin Profile UI Tests added

---
+++ code/app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.goldencarrot"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.goldencarrot"
+        minSdk = 32
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        tasks.withType<Test>{
+            useJUnitPlatform()
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // junit 5
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+
+    //UI tests
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+}
---
+++ code/app/src/test/java/com/example/goldencarrot/UserTest.java
@@ -0,0 +1,59 @@
+package com.example.goldencarrot;
+
+import static com.example.goldencarrot.data.model.user.UserUtils.ADMIN_TYPE;
+import static com.example.goldencarrot.data.model.user.UserUtils.ORGANIZER_TYPE;
+
+import com.example.goldencarrot.data.db.UserRepository;
+import com.example.goldencarrot.data.model.user.User;
+import com.example.goldencarrot.data.model.user.UserImpl;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    private User newUser;
+    private String mockEmail() {
+        return "mock@gmail.com";
+    }
+    private String mockName() {
+        return "BugsBunny";
+    }
+    private FirebaseFirestore db;
+    private UserRepository userRepository;
+    private int userCollectionSize;
+    private User mockUser(String userType) {
+        try {
+            newUser = new UserImpl(mockEmail(), userType, mockName(), null);
+        }
+        catch(Exception e){
+
+        }
+        return newUser;
+    }
+
+    @Test
+    void testCreateUserObj_HappyCase() {
+        User nUser;
+        // test if user is successfully created
+        nUser = mockUser(ADMIN_TYPE);
+        assertSame(nUser.getEmail(), mockEmail());
+        assertSame(nUser.getUserType(), ADMIN_TYPE);
+        assertSame(nUser.getName(), mockName());
+
+        nUser = mockUser(ORGANIZER_TYPE);
+        assertSame(nUser.getUserType(), ORGANIZER_TYPE);
+    }
+
+    @Test
+    void testCreateUserObj_InvalidUserType() {
+        // test if exception is thrown for invalid user type
+        assertThrows(Exception.class, () -> {
+            User nUser = new UserImpl(mockEmail(), "InvalidType", mockName(), null);
+        });
+    }
+
+}
