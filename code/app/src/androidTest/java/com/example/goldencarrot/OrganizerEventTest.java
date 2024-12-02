eac9fea6bf449be4faae87ab52ef8be73db8eb99
Diego Arana <diegoarana@MacBook-Pro-158.local>
2024-12-02 01:13:15 -0700

    [IMPROVEMENT] fixed UI tests

--- code/app/src/androidTest/java/com/example/goldencarrot/OrganizerEventTest.java
+++ code/app/src/androidTest/java/com/example/goldencarrot/OrganizerEventTest.java
@@ -4,8 +4,7 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.Espresso;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -17,23 +16,17 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.net.Uri;
 import android.util.Log;
 
 import com.example.goldencarrot.data.db.EventRepository;
-import com.example.goldencarrot.data.model.event.Event;
 import com.example.goldencarrot.views.OrganizerCreateEvent;
-import com.google.firebase.storage.internal.Sleeper;
 
 @RunWith(AndroidJUnit4.class)
 public class OrganizerEventTest {
